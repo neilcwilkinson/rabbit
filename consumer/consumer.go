de56ddfab5636b03a4ee2fd06ea2d387b5c8fa8c
neilcwilkinson <neilcwilkinson@hotmail.com>
2015-08-18 11:36:33 -0400

    first commit

---
+++ consumer/consumer.go
@@ -0,0 +1,69 @@
+package consumer
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		//log.Fatalf("%s: %s", msg, err)
+		//panic(fmt.Sprintf("%s: %s", msg, err))
+		log.Println("Error:", err)
+	}
+}
+
+func Initialize() {
+	// conn, err := amqp.Dial("amqp://test:letmein@10.5.212.156:5672/")
+	conn, err := amqp.Dial("amqp://fahoobagats:Fytg*pv2c,iCUT@rabbitmq-statsnode-3a5n:15672/")
+	//failOnError(err, "Failed to connect to RabbitMQ")
+
+	if err != nil {
+		//log.Fatalf("%s: %s", msg, err)
+		//panic(fmt.Sprintf("%s: %s", msg, err))
+		log.Println("Error:", err)
+	} else {
+		log.Println("We are connected!!")
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+
+		failOnError(err, "Failed to open a channel")
+		defer ch.Close()
+
+		q, err := ch.QueueDeclare(
+			"hello", // name
+			false,   // durable
+			false,   // delete when usused
+			false,   // exclusive
+			false,   // no-wait
+			nil,     // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+
+		msgs, err := ch.Consume(
+			q.Name, // queue
+			"",     // consumer
+			true,   // auto-ack
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		failOnError(err, "Failed to register a consumer")
+
+		forever := make(chan bool)
+
+		go func() {
+			for d := range msgs {
+				if string(d.Body) == "done" {
+					log.Printf("Received a message: %s", d.Body)
+				}
+			}
+		}()
+
+		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+		<-forever
+	}
+}
